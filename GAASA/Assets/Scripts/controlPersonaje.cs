d4b3ae5f6b4f239d440856d18772b217ccdaa6cb
sergiogomezg30 <al386174@uji.es>
2021-10-28 16:40:32 +0200

    probandoScripts

--- GAASA/Assets/Scripts/controlPersonaje.cs
+++ GAASA/Assets/Scripts/controlPersonaje.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class controlPersonaje : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed;
+    Vector3 target;
+
+    
     void Start()
     {
-        
+        target = transform.position;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0)){
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = 0f;
+        }
+
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.deltaTime);
     }
+
+
 }
