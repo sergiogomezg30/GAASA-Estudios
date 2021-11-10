7867e585088aeca2f9e631b641550060f885dd79
gonzaloRamiaGarcia <al394793@uji.es>
2021-11-10 17:59:00 +0100

    obstaculos spawneados en una pos aleatoria

--- GAASA/Assets/spawnerBehaviour.cs
+++ GAASA/Assets/spawnerBehaviour.cs
@@ -12,13 +12,20 @@ public class spawnerBehaviour : MonoBehaviour
     [SerializeField]
     private float timer;
 
+    public float spawnerMin = -5f;
+    public float spawnerMax = 5f;
+
     // Update is called once per frame
     void Update()
     {
         if (timeUntilSpawn <= 0)
         {
             int randomObstacle = Random.Range(0, obstacles.Length);
-            Instantiate(obstacles[randomObstacle], transform.position, Quaternion.identity);
+            float randOffset = Random.Range(spawnerMin, spawnerMax);
+
+            Instantiate(obstacles[randomObstacle], 
+                new Vector3(transform.position.x,transform.position.y+randOffset, 0f),
+                Quaternion.identity);
             timeUntilSpawn = timer;
         }
 
