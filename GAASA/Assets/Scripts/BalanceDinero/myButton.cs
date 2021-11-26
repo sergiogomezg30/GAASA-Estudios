ba89a5fd0079ecfa918c596c523eb37867f0477b
AdriMon27 <al394498@uji.es>
2021-11-26 22:46:40 +0100

    arreglar ui para que se escale bien

---
+++ GAASA/Assets/Scripts/BalanceDinero/myButton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class myButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] private Sprite offSprite;
+    [SerializeField] private Sprite highlightedSprite;
+    [SerializeField] private Sprite pressedSprite;
+    [SerializeField] private Sprite onSprite;
+
+    private Image actualImage;
+    private bool isOn;
+
+    private void Start()
+    {
+        actualImage = GetComponent<Image>();
+        isOn = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        isOn = !isOn;
+        if (isOn)
+            actualImage.sprite = onSprite;
+        else
+            actualImage.sprite = offSprite;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        actualImage.sprite = highlightedSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (isOn)
+            actualImage.sprite = onSprite;
+        else
+            actualImage.sprite = offSprite;
+
+    }
+}
