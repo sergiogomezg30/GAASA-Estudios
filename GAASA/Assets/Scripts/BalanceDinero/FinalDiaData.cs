044227fe4675e35f4c325f4190653c26c36eb20f
AdriMon27 <90414103+AdriMon27@users.noreply.github.com>
2021-12-09 16:29:52 +0100

    arreglos hechos en clase

--- GAASA/Assets/Scripts/BalanceDinero/FinalDiaData.cs
+++ GAASA/Assets/Scripts/BalanceDinero/FinalDiaData.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class FinalDiaData : MonoBehaviour
 {
-    public static int diaActual;    //se deberia inicializar al principio del juego
-    public static int ahorrosMoney; //se deberia inicializar al principio del juego
+    public static int diaActual = 1;    //se deberia inicializar al principio del juego
+    public static int ahorrosMoney = 1000; //se deberia inicializar al principio del juego
     public static int gastosComida = 10;
 
-    public static bool cocheArreglado;  //se deberia inicializar a TRUE al principio del juego
+    public static bool cocheArreglado = true;  //se deberia inicializar a TRUE al principio del juego
 }
