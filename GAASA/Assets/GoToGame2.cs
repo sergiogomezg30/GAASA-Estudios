bf6551241a8b96cfed31cec112d7d2ff0d3315d1
gonzaloRamiaGarcia <al394793@uji.es>
2021-12-11 00:23:41 +0100

    Dia2

---
+++ GAASA/Assets/GoToGame2.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToGame2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject jugador;
+    public Animator fundidoNegro;
+    public GameObject subirEscaleraAnim;
+    public float aumento = 0.15f;
+    public string nombreJuego;
+
+    //Pequeño pop para resaltar que el objeto es interactuable
+    void OnMouseEnter()
+    {
+        this.transform.localScale += new Vector3(aumento, aumento, 0);
+    }
+
+    void OnMouseExit()
+    {
+        this.transform.localScale -= new Vector3(aumento, aumento, 0);
+    }
+
+    void OnMouseDown()
+    {
+
+        //Si clickeas, se inicia una animacion.
+        this.transform.localScale -= new Vector3(aumento, aumento, 0);
+
+        subirEscaleraAnim.SetActive(true);
+        jugador.SetActive(false);
+        StartCoroutine(CambioEscena());
+
+    }
+
+    IEnumerator CambioEscena()
+    {
+
+        yield return new WaitForSeconds(4);
+        fundidoNegro.SetTrigger("transicion");
+
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(nombreJuego);
+
+    }
+}
