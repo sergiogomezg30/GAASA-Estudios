9a9e7a5e40f4d9c250cdc3cff7534c7e14435541
gonzaloRamiaGarcia <al394793@uji.es>
2021-12-14 09:56:43 +0100

    minijuegoBuscarHerramientas completado

---
+++ GAASA/Assets/SegundoDia.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegundoDia : MonoBehaviour
+{
+    public bool herramientas = false;
+    public GameObject puertaDesbloqueada;
+    public GameObject herramientasUI;
+    public GameObject objetosInteractuables;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (herramientas)
+        {
+            //Desactivamos los objetos con los que aun no hemos interactuado
+            objetosInteractuables.SetActive(false);
+
+            //activamos el icono de las herramientas UI
+            herramientasUI.SetActive(true);
+
+            //abrimos la puerta
+            puertaDesbloqueada.SetActive(true);
+
+        }
+    }
+}
